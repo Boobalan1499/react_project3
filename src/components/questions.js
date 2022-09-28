b8046f0f5cec6584dca87b0661e7498287d5f9a6
Boobalan <boobalanpcivil@gmail.com>
2022-09-28 19:18:21 +0530

    first commit

---
+++ src/components/questions.js
@@ -0,0 +1,27 @@
+export default[
+    {
+        question: "CM of tamilnadu",
+        options:["A", "B", "C", "D"],
+        answer: 1,
+    },
+    {
+        question: "2nd",
+        options:["A", "B", "C", "D"],
+        answer: 0,
+    },
+    {
+        question: "3rd",
+        options:["A", "B", "C", "D"],
+        answer: 3,
+    },
+    {
+        question: "4th",
+        options:["A", "B", "C", "D"],
+        answer: 2,
+    },
+    {
+        question: "5th",
+        options:["A", "B", "C", "D"],
+        answer: 1,
+    }  
+]
